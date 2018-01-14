be0ad8eb85790fd38ae4094416ce6fbc8d9bf0f0
Cloud User <ec2-user@ip-172-31-31-54.ec2.internal>
2018-01-14 12:51:56 +0000

    Ping Check

--- ping_check.py
+++ ping_check.py
@@ -1,6 +1,5 @@
-import commands
-ip='172.31.31.54'
-cmd="ping -c 4 " + ip + "| grep transmitted | awk ' {if ($1==$4)  print 1; else print 0 }'"
-#status,output = commands.getstatusoutput("ping -c 4 ip | grep transmitted | awk ' $1==$4 ? i=1: i=0 {print i}'")
-status,output = commands.getstatusoutput(cmd)
-print ip + ":" +output
+import os
+import subprocess
+
+ret_code = subprocess.call(['ping', '-c', '5', '-W', '3', 'abc987.com'],stdout=open(os.devnull, 'w'),stderr=open(os.devnull, 'w'))
+print ret_code
